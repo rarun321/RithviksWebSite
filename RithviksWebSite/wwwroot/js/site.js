340651d31ea482bd55d22ada492030b9a462980d
rarun321 <rithvikarun24@gmail.com>
2019-08-17 16:04:47 -0700

    Website Remodel

--- RithviksWebSite/wwwroot/js/site.js
+++ RithviksWebSite/wwwroot/js/site.js
@@ -1,1 +1,50 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function () {
+        $("#ProfileImage").hide()
+        $("#Name").hide();
+        $("#SoftwareDeveloper").hide();
+        $("#Logo").hide();
+        $("#About").hide();
+        $("#Projects").hide();
+        $("#Intro").hide();
+        $("#Technologies").hide();
+
+
+
+        $("#PasswordPhrase").hide().fadeIn(1500, function () {
+            TypeEffect();
+        });
+});
+
+
+    var text = "rithvikarun19";
+    var speed = 50;
+    var i = 0;
+
+    function TypeEffect() {
+        if (i < text.length) {
+            document.getElementById("Password").innerHTML += text.charAt(i);
+            i++
+            setTimeout(TypeEffect, speed)
+        }
+        else {
+            $("#Logo").fadeIn(2000, function () {
+                $("#PasswordPhrase").hide()
+                $("#Password").hide()
+                $("#Logo").hide();
+
+                $("#ProfileImage").fadeIn()
+                $("#Name").fadeIn();
+                $("#SoftwareDeveloper").fadeIn(function () {
+                    $("#About").fadeIn(2000)
+                    $("#Projects").fadeIn(2000);
+                    $("#Intro").fadeIn(2000);
+                    $("#Technologies").fadeIn(2000);
+                });
+            })
+        }
+    }
+
+$("#StreamWorkDot").click(function () {
+    $(this).toggleClass("flip");
+    $("#StreamWorkDotInner").toggleClass("flipIn")
+}); 
