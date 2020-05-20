daa03a929c1fbd7ff91b5f6bcaba0d95212a1ab6
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-05-19 18:20:41 -0700

    Semester Updates

---
+++ RithviksWebSite/bin/Release/netcoreapp2.2/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+
+});
+
+
+var text = "";
+var speed = 50;
+var i = 0;
+
+function SetText(t) {
+    text = t
+    TypeEffect();
+}
+
+function TypeEffect() {
+    if (i < text.length) {
+        document.getElementById("Coronado").innerHTML += text.charAt(i);
+        i++
+        setTimeout(TypeEffect, speed)
+    }
+}
+
+function Open(url) {
+    window.open(url);
+}
+
+
+
+//$("#StreamWorkDot").click(function () {
+//    $(this).toggleClass("flip");
+//    $("#StreamWorkDotInner").toggleClass("flipIn")
+//}); 
+
+//function LinkToLinkedIn() {
+
+//}
